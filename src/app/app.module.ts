2c1df070de925d2de76d26e1ffdcba58716d1992
Evans Mwendwa <Evans Mwendwa>
2017-01-16 01:32:33 +0300

    project setup for routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,36 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// import app modules
+import { AppRoutingModule } from './app.routing';
+
+// import components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+// import services
+
+// import pipes
+
+// import directives
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent
+    ],
+    providers: [
+
+    ],
+    exports: [
+        AppRoutingModule
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
