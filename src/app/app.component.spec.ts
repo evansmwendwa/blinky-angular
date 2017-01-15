c1ff99ee01a3ba27d8a590968e4e35ce88d9a8aa
Evans Mwendwa <Evans Mwendwa>
2017-01-16 01:22:02 +0300

    project setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,16 +18,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'Blinky Bill Music'`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('Blinky Bill Music');
   }));
 
   it('should render title in a h1 tag', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    let app = fixture.debugElement.componentInstance;
+    expect(compiled.querySelector('h1').textContent).toContain(app.title);
   }));
 });
