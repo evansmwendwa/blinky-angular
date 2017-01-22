ff605844923040a9e83d5c400e8d89875e37af1f
Evans Mwendwa <evansmwendwa@live.com>
2017-01-22 21:17:31 +0300

    added menu component

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'site-menu',
+    templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+    public links = [
+        {
+            theme:'#a11d77',
+            link:'#',
+            text: 'Music'
+        },
+        {
+            theme:'#313b72',
+            link:'#',
+            text: 'Videos'
+        },
+        {
+            theme:'#f9f200',
+            link:'#',
+            text: 'Radio'
+        },
+        {
+            theme:'#9ed2ce',
+            link:'#',
+            text: 'News'
+        },
+        {
+            theme:'#00aaec',
+            link:'#',
+            text: 'Gigs'
+        },
+        {
+            theme:'#00a48d',
+            link:'#',
+            text: 'Bio'
+        },
+        {
+            theme:'#c885b1',
+            link:'#',
+            text: 'Press'
+        },
+        {
+            theme:'#e1287a',
+            link:'#',
+            text: 'Photos'
+        },
+        {
+            theme:'#3f147b',
+            link:'#',
+            text: 'Hello!'
+        }
+    ];
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
