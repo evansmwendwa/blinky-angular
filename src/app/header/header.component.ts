deae35e19431f53c3bd2a217cd2a84d5775b1fb9
Evans Mwendwa <evansmwendwa@live.com>
2017-01-22 21:16:07 +0300

    manage menuexpander using the menu service

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuService } from '../menu/menu.service';
 
 @Component({
     selector: 'site-header',
@@ -7,15 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-    public menuActive = false;
-
-    constructor() { }
+    constructor(public menuService: MenuService) { }
 
     ngOnInit() {
-    }
-
-    toggleMenu() {
-        this.menuActive = !this.menuActive;
     }
 
 }
