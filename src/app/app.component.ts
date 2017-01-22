ce9c17240e888032f74ef80e02369c9daa8a9720
Evans Mwendwa <evansmwendwa@live.com>
2017-01-22 21:19:22 +0300

    inlcude menu child component in page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuService } from './menu/menu.service';
 
 @Component({
     selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'Blinky Bill Music';
+
+    constructor(public menuService: MenuService) { }
 }
