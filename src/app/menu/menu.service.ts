b20f498c9311794a31f532dbee95c81b34652606
Evans Mwendwa <evansmwendwa@live.com>
2017-01-29 20:19:16 +0300

    close menu when starting navigation

--- src/app/menu/menu.service.ts
+++ src/app/menu/menu.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Router, NavigationStart, Event } from '@angular/router';
+import 'rxjs/add/operator/filter';
 
 @Injectable()
 export class MenuService {
 
     public menuExpanded = false;
 
-    constructor() { }
+    constructor(private router:Router) {
+        router.events.filter(
+            event => event instanceof NavigationStart)
+            .subscribe((event:Event) => { this.menuExpanded = false }
+        );
+    }
 
     toggleMenu() {
         this.menuExpanded = !this.menuExpanded;
