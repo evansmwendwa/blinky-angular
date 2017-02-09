fe3027be21e94510df4dff9c0ceb6562e43ce62b
evansmwendwa <evansmwendwa@live.com>
2017-02-09 12:06:20 +0300

    allow playing audio previews

--- src/app/music/album/album.component.ts
+++ src/app/music/album/album.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { MusicService } from '../music.service';
+import { AudioPlayer } from '../audio-player.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
     selector: 'music-album',
     templateUrl: './album.component.html',
     styleUrls: ['./album.component.scss']
 })
-export class AlbumComponent implements OnInit {
+export class AlbumComponent {
 
-    constructor() { }
+    constructor(
+        public musicService:MusicService,
+        private route: ActivatedRoute,
+        public audioPlayer:AudioPlayer
+    ) {
+        this.route.params.subscribe(params => { this.fetchItem(params) });
+    }
 
-    ngOnInit() {
+    fetchItem(params:any) {
+        this.musicService.getAlbum(params.id);
     }
 
 }
