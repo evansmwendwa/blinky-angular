d825f594c92bd047ed841a6e54fcd64449e48fc1
evansmwendwa <evansmwendwa@live.com>
2017-03-03 03:17:16 +0300

    added support for play queeing and moving to next track

--- src/app/music/audio-player.service.ts
+++ src/app/music/audio-player.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { ConfigService } from '../shared';
+import { TracksService } from './tracks.service';
+
 
 @Injectable()
 export class AudioPlayer {
@@ -6,15 +9,53 @@ export class AudioPlayer {
     public player:any;
     public src:string = '';
     public playing:boolean = false;
+    public playListIndex = -1;
 
-    constructor() {
+    constructor(
+        public config: ConfigService,
+        public tracksService: TracksService
+    ) {
         this.player = new Audio();
         let self = this;
+
         this.player.onpause = function() {
-            //self.player.src = '';
-            //self.src = '';
             self.playing = false;
         }
+
+        this.player.onended = function() {
+            self.playNext();
+        }
+    }
+
+    playNext() {
+        let count = this.tracksService.tracks.length;
+        if(count < 1) {
+            return;
+        }
+
+        let next = this.playListIndex + 1;
+
+        if(next >= count) {
+            next = 0;
+        }
+
+        this.playTrack(next);
+    }
+
+    playTrack(index:number) {
+        let track = this.tracksService.tracks[index];
+
+        if(typeof track !== 'undefined') {
+            this.playing = true;
+            if(index === this.playListIndex) {
+                this.player.play();
+                return;
+            }
+
+            this.play(track.stream_url + '?client_id=' + this.config.soundCloudClient);
+            this.playListIndex = index;
+        }
+
     }
 
     play(file?:string) {
