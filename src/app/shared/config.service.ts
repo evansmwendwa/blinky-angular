6a1562f50a9c7e6f9c713d3e23000c876823715e
evansmwendwa <evansmwendwa@live.com>
2017-03-03 03:15:08 +0300

    added soundcloud endpoints

--- src/app/shared/config.service.ts
+++ src/app/shared/config.service.ts
@@ -3,6 +3,8 @@ import { Injectable, isDevMode } from '@angular/core';
 @Injectable()
 export class ConfigService {
     public baseUrl = 'http://backend.blinkybillmusic.com/api';
+    public soundCloudApi = 'https://api.soundcloud.com/users/4496565/tracks';
+    public soundCloudClient = 'c7p0XZnTQsLUH5pwVTuhZPlZPvpCKHVE';
 
     constructor() {
         if(isDevMode()) {
